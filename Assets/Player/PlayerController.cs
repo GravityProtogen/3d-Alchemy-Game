e3a991f78344e73c817e3ff640b9811072b1ed24
GravityProtogen <pedroarturcosta@hotmail.com>
2026-05-19 16:56:15 -0300

    Troca de Modelo, Animação Tree, Movimento e Colisão

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,32 +1,57 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+
     private Vector2 move;
+    private Animator animator;
+
+    private float currentSneakValue;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        movePlayer();
+        MovePlayer();
     }
 
-    public void movePlayer()
+    public void MovePlayer()
     {
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
-        
+        Vector3 movement = new Vector3(move.x, 0f, move.y).normalized;
+
+        // Rotação
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(movement),
+                0.15f
+            );
+        }
+
+        // Movimento
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        // Valor alvo do Animator
+        float targetValue = movement.magnitude > 0 ? 1f : 0f;
+
+        // Suaviza transição (passa pelo 0.5 naturalmente)
+        currentSneakValue = Mathf.Lerp(
+            currentSneakValue,
+            targetValue,
+            Time.deltaTime * 5f
+        );
+
+        animator.SetFloat("SneakingSpeed", currentSneakValue);
     }
 }
